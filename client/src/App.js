6b1a70ada6aa3d64a2c6b3735441d52bf8234eab
sam <sam@samspain.com>
2021-08-07 13:07:57 +0100

    Send message from text box to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import ChatBox from "./ChatBox";
+import './App.css';
 
 const client = new W3CWebSocket("ws://127.0.0.1:8081");
 
@@ -26,6 +27,9 @@ class App extends Component {
         console.log("All messages received");
         console.log(message);
         this.setState({ text: JSON.parse(data.content) });
+      } else if (data.type === "createMessage") {
+        let newChats = this.state.text.push(JSON.parse(data.content));
+        this.setState({ text: newChats });
       }
     };
     this.handleTextChange = this.handleTextChange.bind(this);
@@ -34,8 +38,9 @@ class App extends Component {
   handleTextChange(e) {
     if (e.keyCode === 13) {
       client.send(
-        JSON.stringify({ type: "createMessage", content: "Text change" })
+        JSON.stringify({ type: "createMessage", content: e.target.value })
       );
+      this.setState({ inputText: "" });
     } else {
       this.setState({ inputText: e.target.value });
     }
@@ -43,12 +48,16 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <NumberList numbers={this.state.text} />
+      <div class="row">
+        <div class="column">
+          <NumberList numbers={this.state.text} />
+        </div>
+        <div class="column">
         <ChatBox
           text={this.state.inputText}
           handleTextChange={this.handleTextChange}
         />
+        </div>
       </div>
     );
   }
